dc2c0fd8b0acc46d70bfed9890389fc371c8ee40
Bela-Szilagyi <szilagyib.mail@gmal.com>
2017-09-21 12:34:40 +0200

    outcommented getline version

--- main.cpp
+++ main.cpp
@@ -5,7 +5,12 @@ using namespace std;
 int main(int argc, char *argv[]) {
     cout << "Please enter your name: ";
     string name;
-    cin >> name; // or getline(cin, name); ??? which one is better???
+    cin >> name;
     cout << "Hello " << name << "!" << endl;
+    /* OR
+    getline(cin, name);
+    if (name.empty()) cout << "Hello, World!" << endl;
+    else cout << "Hello " << name << "!" << endl;
+    */
     return 0;
 }
