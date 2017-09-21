400e469246e0ec8503de6505eb75d6fb84bfe011
Bela-Szilagyi <szilagyib.mail@gmal.com>
2017-09-21 12:23:47 +0200

    add comment about getline

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(int argc, char *argv[]) {
     cout << "Please enter your name: ";
     string name;
-    cin >> name;
+    cin >> name; // or getline(cin, name); ??? which one is better???
     cout << "Hello " << name << "!" << endl;
     return 0;
 }
